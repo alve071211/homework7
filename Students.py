d6b4c138d43b18bfdfbb26bd8a93908a69315856
alve071211 <alve071211@gmail.com>
2024-09-16 10:16:56 +1200

    Add files via upload

---
+++ Students.py
@@ -0,0 +1,7 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades_sum=sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])
+sorted_students=sorted(students)
+gpa={}
+gpa_1=dict(zip(sorted_students,grades_sum))
+print(gpa_1)
